4a67c8ffd437bc7073889de2ab166059c20790e0
Brytvn <105264592+BryanTeven@users.noreply.github.com>
2024-04-26 23:10:36 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function lempardadu() {
+    const jumlahdadu = document.getElementById("jumlahdadu").value;
+    const hasil = document.getElementById("hasil");
+    const gambardadu = document.getElementById("gambardadu");
+    const values = [];
+    const gambar = [];
+
+    for (let i = 0; i < jumlahdadu; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        gambar.push(`<img src="./img/${value}.jpg" alt=" Dadu ${value}">`);
+
+    }
+
+    hasil.textContent = `Dadu: ${values.join(', ')}`;
+    gambardadu.innerHTML = gambar.join('');
+
+}
